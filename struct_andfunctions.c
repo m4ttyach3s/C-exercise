e4fd173729a179b5f096b27200d6d187ae4891e1
m4ttyach3s <80849019+m4ttyach3s@users.noreply.github.com>
2021-03-24 00:30:10 +0100

    Update struct_andfunctions.c

--- struct_andfunctions.c
+++ struct_andfunctions.c
@@ -3,23 +3,19 @@
 #define size 100
 
 //---defining my struct---//
-
 struct studenti{
 	char nome[size], cognome[size];
 	int anni;
 	};
-
 //---function area---//
-
 struct studenti trovastud();
 void Stampa(struct studenti s1);
 void looping_call();
 void callback(struct studenti s1);
 
-
 //---output of my results---//
 void callback(struct studenti s1){
- 	 trovastud(s1);
+ 	 s1=trovastud();
  	 Stampa(s1); 
 }
 
@@ -40,7 +36,7 @@ void Stampa(struct studenti s1){
 	printf("\nCognome: %s", s1.cognome);
 	printf("\nAnni: %i\n", s1.anni);
 	}
-
+	
 struct studenti trovastud(){
 	struct studenti s2;
 	printf("\nInserisci il nome studente: ");
@@ -51,13 +47,12 @@ struct studenti trovastud(){
 	scanf(" %i", &s2.anni);
 	return s2;
 }
-
 //---end of function area--//
-	
 int main(void){
  	struct studenti s1;
  	struct studenti s3;
  	struct studenti s4;
+ 	
  	s1 = trovastud();
  	Stampa(s1); 
  	s1.nome[size]='\0'; //emptying my array nome
